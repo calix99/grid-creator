4db1430a724d89a1e79e28c705cfd20a1d3a76a0
calix <7461827@qq.com>
2023-04-20 12:39:34 +0800

    ui基本完成

--- code.ts
+++ code.ts
@@ -6,7 +6,13 @@
 // full browser environment (see documentation).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+// figma.showUI(
+//   "<b>Hello from Figma</b>",
+//   { width: 400, height: 800, title: "My title" }
+// )
+figma.showUI(__html__,{width:348,height:600});
+
+
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
